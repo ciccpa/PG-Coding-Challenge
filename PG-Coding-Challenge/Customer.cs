129037021d0228602d080a67ed56bd6321d78b82
Peter Ciccone <peteoc98@yahoo.com>
2020-11-14 11:35:18 -0500

    first commit

---
+++ PG-Coding-Challenge/Customer.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG_Coding_Challenge
+{
+    public class Customer
+    {
+        // Member variables
+        private string m_Name;
+        int m_Age;
+        private string m_Gender;
+        private string m_Condition;
+
+        //****************************************************
+        // Method: Customer()
+        //
+        // Default constructor. Fills member variables with
+        //                               default data.
+        //****************************************************
+        public Customer()
+        {
+            m_Name = "NO_NAME";
+            m_Gender = "NO_GENDER";
+            m_Condition = "NO_CONDITION";
+            m_Age = 0;
+        }
+
+        //****************************************************
+        // Method: toString()
+        //
+        // Purpose: Formats member variable data to be printed on screen.
+        //****************************************************
+        public override string ToString()
+        {
+            return m_Name + ", " + m_Age + ", " + m_Gender + ", " + m_Condition + "\r";
+        }
+
+        //****************************************************
+        // Method: CalculateEstimate()
+        //
+        // Calculates estimate based on the given criteria.
+        //****************************************************
+        public double CalculateEstimate()
+        {
+            // Check age first
+            if(m_Age < 18)
+            {
+                Console.WriteLine("Invalid input. Must be at least 18.");
+                return 0;
+            }
+
+            // Base annual cost
+            double cost = 100;
+
+            // Check and keep track of age every 5 yrs
+            int counter = 5, i = 0;
+            while(m_Age > 18 + counter)
+            {
+                i++;
+                counter += 5;
+            }
+
+            // Multiply # of occurrences of 5 yrs * $20 additional fee
+            double baseIncrease = i * 20;
+            cost += baseIncrease;
+         
+            // Cost increase percentage set to 0.
+            // This is in case the user has no underlying conditions
+            double costIncrease = 0.0;
+            // Set string to all caps to avoid case sensitivity
+            m_Condition = m_Condition.ToUpper();
+            m_Gender = m_Gender.ToUpper();
+          
+            // Health conditions specified here, used if statement
+            // for sake of time. Could have used switch also.
+            if (m_Condition == "ALLERGIES")
+            {
+                costIncrease = 1.01;
+                cost = cost * costIncrease;
+            } 
+            else if (m_Condition == "SLEEP APNEA")
+            {
+                costIncrease = 1.06;
+                cost = cost * costIncrease;
+            }
+            else if (m_Condition == "HEART DISEASE")
+            {
+                costIncrease = 1.17;
+                cost = cost * costIncrease;
+            }
+            
+            // Check gender last
+            if (m_Gender == "FEMALE")
+            {
+                cost -= 12;
+            }
+            return cost;
+        }
+
+        // Member variables get/set 
+        public string Name
+        {
+            get { return m_Name; }
+            set { m_Name = value; }
+        }
+
+        public int Age
+        {
+            get { return m_Age; }
+            set { m_Age = value; }
+        }
+        public string Gender
+        {
+            get { return m_Gender; }
+            set { m_Gender = value; }
+        }
+
+        public string Condition
+        {
+            get { return m_Condition; }
+            set { m_Condition = value; }
+        }
+    }
+}
---
+++ PG-Coding-Challenge/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace PG_Coding_Challenge
+{
+    class Program
+    {
+         //****************************************************
+        // Method: Main
+        //
+        // Create instance of Customer class, assigns data.
+        // Displays menu to let the user choose which option they want to perform.
+        //****************************************************
+        static void Main(string[] args)
+        {
+            Customer c = new Customer();
+
+            int userChoice = 0;
+            while (userChoice != 4)
+            {
+                Console.WriteLine("Customer Menu");
+                Console.WriteLine("------------");
+                Console.WriteLine("1 - Enter Customer Information");
+                Console.WriteLine("2 - Calculate estimate");
+                Console.WriteLine("3 - Display customer information");
+                Console.WriteLine("4 - Exit");
+                Console.WriteLine("Enter Choice:");
+                String menuChoice = Console.ReadLine();
+                userChoice = Convert.ToInt32(menuChoice);
+
+                if (userChoice == 1)
+                {
+                    Console.Write("Enter name: ");
+                    string name = Console.ReadLine();
+                    c.Name = name;
+
+                    Console.Write("Enter age: ");
+                    string age = Console.ReadLine();
+                    int inputAge = Convert.ToInt32(age);
+                    c.Age = inputAge;
+
+                    Console.Write("Enter gender: ");
+                    string gender = Console.ReadLine();
+                    c.Gender = gender;
+
+                    Console.Write("Enter condition (enter 'none' if none): ");
+                    string condition = Console.ReadLine();
+                    c.Condition = condition;
+                }
+
+                if (userChoice == 2)
+                {
+                    Console.WriteLine("Calculated Estimate: $" + c.CalculateEstimate().ToString("N2"));
+                }
+
+                if (userChoice == 3)
+                {
+                    Console.WriteLine(c.ToString());
+                }
+            }
+        }
+    }
+}
